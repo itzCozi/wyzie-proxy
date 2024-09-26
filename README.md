f70688fa1c497513fc0f24e514b0e4b8d2eb8807
Cooper <Cooperransom08@gmail.com>
2024-09-26 16:42:23 +0000

    Update proxy

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-find it at http://proxy.wyzie.ru !
+# Wyzie Proxy
+Wyzie Proxy is a simple proxy server that can be used to bypass internet restrictions and access blocked websites.  
+<sup>
+  written in Typescript using <a href="https://nitro.unjs.io">Nitro</a>.
+</sup>
+
+find it at [proxy.wyzie.ru](http://proxy.wyzie.ru) or [self-host it](https://deploy.workers.cloudflare.com/?url=https://github.com/itzcozi/wyzie-proxy) for **free**!
+
+<hr />
+<sup>
+  Apart of the <a href="https://wyzie.ru">wyzie toolset</a> made by BadDeveloper.
+</sup>
--- src/routes/[...url].ts
+++ src/routes/[...url].ts
@@ -1,13 +1,9 @@
 import { getBodyBuffer } from "@/utils/body";
 import { getProxyHeaders, getAfterResponseHeaders, getBlacklistedHeaders } from "@/utils/headers";
 
-// TODO: Fix the issue when  the user goes to https://proxy.wyzie.ru/reddit.com and clicks on a link and then gets taken to https://proxy.wyzie.ru/r/midlyinfuriating which doesnt exist, we need to fix routing.
-
 export default defineEventHandler(async (event) => {
   // handle cors, if applicable
   if (isPreflightRequest(event)) return handleCors(event, {});
-
-  // Extract destination from router param or query parameter
   let destination: string =
     getRouterParam(event, "url") || getQuery<{ destination?: string }>(event).destination || "";
   const body = await getBodyBuffer(event);
